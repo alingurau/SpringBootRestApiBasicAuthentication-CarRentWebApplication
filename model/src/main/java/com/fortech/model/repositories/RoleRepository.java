46026e0a064fdbb9b5996b76fc3925c066578b51
Alin Gurau <alin.gurau@emanuel.ro>
2018-08-10 13:55:44 +0300

    creating RoleRepository interface

--- model/src/main/java/com/fortech/model/repositories/RoleRepository.java
+++ model/src/main/java/com/fortech/model/repositories/RoleRepository.java
@@ -1,10 +1,7 @@
-package com.fortech.serviceapi;
+package com.fortech.model.repositories;
 
 import com.fortech.model.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-    RoleEntity findByRole(String role);
 }
