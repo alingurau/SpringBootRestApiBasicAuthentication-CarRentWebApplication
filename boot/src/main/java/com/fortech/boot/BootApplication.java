7b36f70ac3a9eaf9935113fffc1544a74a6d8623
Alin Gurau <alin.gurau@emanuel.ro>
2018-09-04 14:26:27 +0300

    refactoring the entity class

--- boot/src/main/java/com/fortech/boot/BootApplication.java
+++ boot/src/main/java/com/fortech/boot/BootApplication.java
@@ -3,12 +3,13 @@ package com.fortech.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.fortech")
 @EntityScan("com.fortech.model")
-//@ComponentScan({"com.fortech.model","com.fortech.restapi","com.fortech.restapiimpl","com.fortech.serviceapi","com.fortech.serviceapiimpl"})
+@ComponentScan({"com.fortech.model","com.fortech.restapi","com.fortech.restapiimpl","com.fortech.serviceapi","com.fortech.serviceapiimpl"})
 public class BootApplication{
 
     public static void main(String[] args) {
